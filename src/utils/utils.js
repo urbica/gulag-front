e8ec6e4f284ccf0fcbb493b49387dfcfff72eafc
Andrey Bakhvalov <bakhvalov.andrey@gmail.com>
2017-10-13 14:09:39 +0300

    fix: change http to https

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -12,12 +12,12 @@ export const groupById = arr => (
 
 export const getData = () => (
   Promise.all([
-    fetch('http://gulagmap.ru/api/public/camps.json', options).then(res => res.json()),
-    fetch('http://gulagmap.ru/api/public/uploads.json', options).then(r => r.json()),
-    fetch('http://gulagmap.ru/api/public/activities.json', options).then(r => r.json()),
-    fetch('http://gulagmap.ru/api/public/places.json', options).then(r => r.json()),
-    fetch('http://gulagmap.ru/api/public/types.json', options).then(r => r.json()),
-    fetch('http://gulagmap.ru/api/public/periods.json', options).then(r => r.json())
+    fetch('https://gulagmap.ru/api/public/camps.json', options).then(res => res.json()),
+    fetch('https://gulagmap.ru/api/public/uploads.json', options).then(r => r.json()),
+    fetch('https://gulagmap.ru/api/public/activities.json', options).then(r => r.json()),
+    fetch('https://gulagmap.ru/api/public/places.json', options).then(r => r.json()),
+    fetch('https://gulagmap.ru/api/public/types.json', options).then(r => r.json()),
+    fetch('https://gulagmap.ru/api/public/periods.json', options).then(r => r.json())
   ])
     .then(([prisons, photos, activities, places, types, periods]) => ({
       prisons: groupById(prisons),

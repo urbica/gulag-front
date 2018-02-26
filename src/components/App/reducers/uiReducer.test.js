2be306385e102f5d2f5ec1599f6a28fe5dd2e3d4
Andrey Bakhvalov <bakhvalov.andrey@gmail.com>
2018-02-27 02:13:36 +0300

    test: add tests for ui reducer
    #44

--- src/components/App/reducers/uiReducer.test.js
+++ src/components/App/reducers/uiReducer.test.js
@@ -1,34 +1,95 @@
-import Immutable from 'immutable';
-import reducer, { changeCurrentYear } from './uiReducer';
+import Immutable, { Map } from 'immutable';
+import reducer, {
+  changeCurrentYear,
+  changeViewport,
+  toggleDemo,
+  toggleAllPrisons,
+  toggleMenu,
+  toggleCampFilters
+} from './uiReducer';
+import { FETCH_REQUEST, FETCH_SUCCESS } from './dataReducer';
+
+let initState = Immutable.fromJS({
+  currentYear: null,
+  viewport: {
+    latitude: null,
+    longitude: null,
+    zoom: null
+  },
+  isDemoPlay: null,
+  isShowAllPrisons: null,
+  isMenuOpen: null,
+  isDataLoading: null,
+  isCampFiltersOpen: null
+});
 
 describe('Ui Reducer', () => {
   it('returns a state object', () => {
-    const result = reducer(undefined, { type: 'ANYTHING' });
+    initState = reducer(undefined, { type: 'ANYTHING' });
 
-    expect(result).toBeDefined();
+    expect(initState).toBeDefined();
   });
-
   it('change current year', () => {
-    const startState = Immutable.Map({
-      currentYear: null,
-      currentPrison: null,
-      currentPeriod: null,
-      isSearchOpen: false,
-      isAboutOpen: false,
-      isDemoPlay: false,
-      isShowAllPrisons: false
-    });
-    const expectedState = Immutable.Map({
-      currentYear: 1937,
-      currentPrison: null,
-      currentPeriod: null,
-      isSearchOpen: false,
-      isAboutOpen: false,
-      isDemoPlay: false,
-      isShowAllPrisons: false
-    });
+    const value = 1937;
+    const expectedState = initState
+      .set('currentYear', value)
+      .set('isShowAllPrisons', false);
 
-    const result = reducer(startState, changeCurrentYear(1937));
-    expect(result).toEqual(expectedState);
+    initState = reducer(initState, changeCurrentYear(value));
+    expect(initState).toEqual(expectedState);
+  });
+  it('change viewport', () => {
+    const expectedState = initState.set('viewport', Map({
+      latitude: 70,
+      longitude: 50,
+      zoom: 3
+    }));
+
+    initState = reducer(initState, changeViewport({
+      latitude: 70,
+      longitude: 50,
+      zoom: 3
+    }));
+    expect(initState).toEqual(expectedState);
+  });
+  it('toggle demo', () => {
+    const expectedState = initState
+      .set('isDemoPlay', true)
+      .set('isShowAllPrisons', false);
+
+    initState = reducer(initState, toggleDemo());
+    expect(initState).toEqual(expectedState);
+  });
+  it('can show all prisoners', () => {
+    const expectedState = initState
+      .set('isDemoPlay', false)
+      .set('isShowAllPrisons', true);
+
+    initState = reducer(initState, toggleAllPrisons());
+    expect(initState).toEqual(expectedState);
+  });
+  it('toggle menu', () => {
+    const expectedState = initState.set('isMenuOpen', true);
+
+    initState = reducer(initState, toggleMenu());
+    expect(initState).toEqual(expectedState);
+  });
+  it('can show that data is loading', () => {
+    const expectedState = initState.set('isDataLoading', true);
+
+    initState = reducer(initState, { type: FETCH_REQUEST });
+    expect(initState).toEqual(expectedState);
+  });
+  it('can show that data is loaded', () => {
+    const expectedState = initState.set('isDataLoading', false);
+
+    initState = reducer(initState, { type: FETCH_SUCCESS });
+    expect(initState).toEqual(expectedState);
+  });
+  it('toggle camp filters', () => {
+    const expectedState = initState.set('isCampFiltersOpen', true);
+
+    initState = reducer(initState, toggleCampFilters());
+    expect(initState).toEqual(expectedState);
   });
 });

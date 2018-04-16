5a48fd31e5051428683a9ecfbfe642437a6a1873
Anton Ivchenko <a.a.ivchenko@gmail.com>
2018-04-16 12:46:42 +0300

    feat: add hover for link with email

--- src/components/About/Link.js
+++ src/components/About/Link.js
@@ -6,8 +6,13 @@ export default styled.a`
   padding-bottom: 14px;
   border: 1px solid rgba(226, 243, 227, 0.5);
   margin: 40px 0;
-
   color: rgba(226, 243, 227, 0.8);
   text-align: center;
   text-decoration: none;
+  transition: 0.5s;
+
+  &:hover {
+    background: #e2f3e3;
+    color: #000000;
+  }
 `;

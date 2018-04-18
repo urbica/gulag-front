c83e8d3738852babe3ddbe25dd1aa438ab9bb3fc
Andrey Bakhvalov <bakhvalov.andrey@gmail.com>
2018-04-18 16:19:23 +0300

    ci: move from gulag-next.urbica to gulag.urbica

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 set -e
 
-if [ -d /mnt/storage/urbica/gulag/gulag-next ]; then
-    cd /mnt/storage/urbica/gulag/gulag-next
+if [ -d /mnt/storage/urbica/gulag/front ]; then
+    cd /mnt/storage/urbica/gulag/front
     git pull
 else
     cd /mnt/storage/urbica
     mkdir -p /mnt/storage/urbica/gulag
     cd /mnt/storage/urbica/gulag
     ssh-keyscan -p 2222 gitlab.urbica.co >> ~/.ssh/known_hosts
-    git clone -b next-version ssh://git@gitlab.urbica.co:2222/gulag/gulagmap.git gulag-next
-    cd gulag-next
+    git clone -b dev ssh://git@gitlab.urbica.co:2222/gulag/front.git front
+    cd front
 fi
 
 docker-compose pull
 docker-compose up -d --force-recreate
-# docker-compose exec gulag-next-api npm run db:migrate

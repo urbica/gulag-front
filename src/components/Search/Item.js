91294a4ce7e1d01b0ed26a25c0c9208d9bc9a982
Anton Ivchenko <a.a.ivchenko@gmail.com>
2018-04-20 19:10:37 +0300

    fix: add hover for search result
    #122

--- src/components/Search/Item.js
+++ src/components/Search/Item.js
@@ -5,4 +5,10 @@ export default styled.div`
   border-bottom: 1px solid rgba(226, 243, 227, 0.1);
   margin-bottom: 20px;
   cursor: pointer;
+  opacity: 0.7;
+  transition: 0.5s;
+
+  &:hover {
+    opacity: 1;
+  }
 `;

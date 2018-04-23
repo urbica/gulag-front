720816cde2abf1bfba8b54603cc7dff9de2bffce
Andrey Bakhvalov <bakhvalov.andrey@gmail.com>
2018-04-23 20:41:26 +0300

    fix: photos subtitle styles
    #124

--- src/components/CampCard/Bottom.js
+++ src/components/CampCard/Bottom.js
@@ -3,5 +3,4 @@ import styled from 'styled-components';
 export default styled.div`
   width: 100%;
   padding-top: 40px;
-  border-top: 1px solid rgba(219, 235, 219, 0.2);
 `;
--- src/components/CampCard/PrisonDescription/Gallery/Gallery.styled.js
+++ src/components/CampCard/PrisonDescription/Gallery/Gallery.styled.js
@@ -4,6 +4,8 @@ export default styled.section`
   display: flex;
   flex-wrap: wrap;
 
+  margin-top: 15px;
+
   img {
     height: 250px;
     margin: 0 1rem 1rem 0;

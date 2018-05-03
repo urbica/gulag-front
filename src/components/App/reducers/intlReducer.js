06ee7ef8cdbb433208fd98961748386c333cb9d1
Andrey Bakhvalov <bakhvalov.andrey@gmail.com>
2018-05-03 12:24:29 +0300

    fix: russian was settled as a default language
    #172

--- src/components/App/reducers/intlReducer.js
+++ src/components/App/reducers/intlReducer.js
@@ -2,28 +2,29 @@ import Immutable from 'immutable';
 import { UPDATE } from 'react-intl-redux';
 
 import ruLocale from '../../../intl/ru';
-import enLocale from '../../../intl/en';
+// import enLocale from '../../../intl/en';
 
-const locales = {
-  ru: ruLocale,
-  'ru-RU': ruLocale,
-  en: enLocale,
-  'en-US': enLocale
-};
+// const locales = {
+//   ru: ruLocale,
+//   'ru-RU': ruLocale,
+//   en: enLocale,
+//   'en-US': enLocale
+// };
 
-const { language, languages } = window.navigator;
-let defaultLocale = locales[language] ? locales[language] : enLocale;
+// const { language, languages } = window.navigator;
+// let defaultLocale = locales[language] ? locales[language] : enLocale;
+//
+// if (languages) {
+//   for (let i = 0; i < languages.length; i += 1) {
+//     if (locales[languages[i]]) {
+//       defaultLocale = locales[languages[i]];
+//       break;
+//     }
+//   }
+// }
 
-if (languages) {
-  for (let i = 0; i < languages.length; i += 1) {
-    if (locales[languages[i]]) {
-      defaultLocale = locales[languages[i]];
-      break;
-    }
-  }
-}
-
-const initialState = Immutable.fromJS(defaultLocale);
+// const initialState = Immutable.fromJS(defaultLocale);
+const initialState = Immutable.fromJS(ruLocale);
 
 export default (state = initialState, action) => {
   if (action.type !== UPDATE) {

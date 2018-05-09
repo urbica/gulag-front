1ef5a138a3d489383c8d00c5436315c4e391604b
Anna Barinova <anna@urbica.co>
2018-05-09 13:20:09 +0000

    hot fix

--- src/components/Map/Container.js
+++ src/components/Map/Container.js
@@ -6,7 +6,7 @@ export default styled.div`
   bottom: 0;
 
   width: 100%;
-  hight: 100vh;
+  height: 100vh;
 
   transform: translateY(${({ slideUp }) => (slideUp ? '-30%' : '0')});
   transition: 0.4s;

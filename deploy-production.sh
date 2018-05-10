528791dacbf8d8e7256f8d0503b6cdac2b0733da
Stepan Kuzmin <to.stepan.kuzmin@gmail.com>
2018-05-10 10:53:08 +0300

    ci: setup deploy to production

--- deploy-production.sh
+++ deploy-production.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 set -e
 
-cd /home/docker/gulag
-git pull
+REPO_URL=ssh://git@gitlab.urbica.co:2222/gulag/front.git
+REPO_PATH=/home/docker/gulag/front
+
+if [ -d $REPO_PATH ]; then
+    cd $REPO_PATH
+    git pull
+else
+    mkdir -p $REPO_PATH
+    cd $REPO_PATH/../
+    ssh-keyscan -p 2222 gitlab.urbica.co >> ~/.ssh/known_hosts
+    git clone $REPO_URL
+    cd $REPO_PATH
+fi
+
 docker-compose -f docker-compose.production.yml pull
 docker-compose -f docker-compose.production.yml up -d
